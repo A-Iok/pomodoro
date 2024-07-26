61a86a6bfcb1642a81ca1ef411c5c9d3a2bfd83f
A-Iok <firecerambycidae625@gmail.com>
2024-07-26 23:01:36 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function ImageButton() {
+
+  const [image, setImage] = useState(null);
+
+  const onClick = checked => {
+    fetch('https://picsum.photos/800', {
+      method: 'GET',
+      mode: "cors"
+    })
+      .then(res => res.blob())
+      .then(blob => {
+        setImage([URL.createObjectURL(blob)]);
+        console.log(URL.createObjectURL(blob));
+      })
+      .then(data => {
+        // .thenは成功した時の処理を示す場合に使う。
+        console.log('Success');
+      })
+      .catch((error) => {
+        // .catchは失敗の時の処理を示す場合に使う。
+        console.error('Error:', error);
+      });
+
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="panel">
+      <button onClick={onClick}>
+        画像取得
+      </button>
+      <img src={image} width="800px" />
     </div>
   );
 }
+
+function App() {
+  return (
+    <div className="container is-fluid">
+      <ImageButton />
+    </div>
+  );
+}
+
+const root = document.getElementById('root');
+//ReactDOM.render(<App />, root);
 
 export default App;
